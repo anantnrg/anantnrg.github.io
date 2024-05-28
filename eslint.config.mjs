788a55a42232986673858d6f88d9678a6d3ab574
Anant Narayan <anantnrg@gmail.com>
2024-05-28 14:09:08 +0530

    add eslint confi

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs';
+
+export default withNuxt({
+  rules: {
+    semi: ["error", "always"],
+    "prettier/prettier": ["error"],
+    "eqeqeq": "error",
+    "no-console": "warn",
+    "react/display-name": "off",
+    "react/no-children-prop": "off",
+    "react/react-in-jsx-scope": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "import/no-unresolved": ["off"],
+    "react/destructuring-assignment": "off",
+    "react/jsx-filename-extension ": "off",
+    "import/prefer-default-export": "off",
+    "react/jsx-props-no-spreading": "off",
+    "import/extensions": "off"
+  }
+});
