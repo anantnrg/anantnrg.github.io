098b9f433dd814ea70f8c4766ae6e8eda3cae44c
Anant Narayan <anantnrg@gmail.com>
2024-05-28 15:33:36 +0530

    start working on stuff

--- app.vue
+++ app.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>
+  <div class="w-full h-full flex flex-col">
     <NuxtPage />
   </div>
 </template>
+
+<style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+html,body {
+  width: 100%;
+  height: 100%;
+}
+</style>
