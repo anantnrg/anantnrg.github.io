f9038faeb5b9f36a23262650985536e716f02a6a
Anant Narayan <anantnrg@gmail.com>
2024-05-29 17:21:47 +0530

    hmmm

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,14 +1,16 @@
 <template>
-    <div class="w-full h-[64px] px-10 bg-mantle border-b-surface0 border-b-1 flex items-center justify-between">
-        <div class=""><span class="font-spaceGrotesk font-600 text-3xl text-accent">Anant Narayan</span></div>
+    <header class="bg-mantle/75 backdrop-blur border-b border-surface0 sticky top-0 z-50">
+    <div class="mx-auto h-[64px] px-8 flex items-center justify-between gap-4">
+        <div class="flex-1"><span class="font-spaceGrotesk font-600 text-3xl text-accent">Anant Narayan</span></div>
         <div class="w-auto h-full items-center justify-center flex">
             <div class="mr-6"><a href="#" class="font-sans font-400 text-text decoration-none">Home</a></div>
             <div class="mr-6"><a href="#" class="font-sans font-400 text-text decoration-none">Blog</a></div>
             <div class="mr-6"><a href="#" class="font-sans font-400 text-text decoration-none">Project Series</a></div>
             <div><a href="#" class="font-sans font-400 text-text decoration-none">About</a></div>
         </div>
-        <div class="">
+        <div class="flex-1 items-center justify-end">
             <a href="https://github.com/anantnrg"><Icon name="octicon:mark-github-24" class="text-text"/></a>
         </div>
     </div>
+</header>
 </template>
