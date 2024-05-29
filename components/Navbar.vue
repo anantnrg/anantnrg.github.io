1b812dd5867fefc4de5ebce192bfec93d510f678
Anant Narayan <anantnrg@gmail.com>
2024-05-29 21:39:00 +0530

    cant find a fucking accent color

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,12 +1,12 @@
 <template>
-    <header class="bg-mantle/75 backdrop-blur border-0 border-b border-solid border-surface0 sticky top-0 z-50">
+    <header class="bg-base/75 backdrop-blur border-0 border-b border-solid border-surface0 sticky top-0 z-50">
     <div class="mx-auto h-[64px] px-8 flex items-center justify-between gap-4">
         <div class="flex-1"><span class="font-spaceGrotesk font-600 text-3xl text-accent">Anant Narayan</span></div>
         <div class="w-auto h-full items-center justify-center flex">
-            <div class="mr-6"><a href="#" class="font-spaceGrotesk font-400 text-text decoration-none">Home</a></div>
-            <div class="mr-6"><a href="#" class="font-spaceGrotesk font-400 text-text decoration-none">Blog</a></div>
-            <div class="mr-6"><a href="#" class="font-spaceGrotesk font-400 text-text decoration-none">Project Series</a></div>
-            <div><a href="#" class="font-sans font-400 text-text decoration-none">About</a></div>
+            <div class="mr-6"><a href="#" class="font-sans font-semibold text-text text-sm decoration-none hover:text-accent">Home</a></div>
+            <div class="mr-6"><a href="#" class="font-sans font-semibold text-text text-sm decoration-none hover:text-accent">Blog</a></div>
+            <div class="mr-6"><a href="#" class="font-sans font-semibold text-text text-sm decoration-none hover:text-accent">Project Series</a></div>
+            <div><a href="#" class="font-sans font-semibold text-text text-sm decoration-none hover:text-accent">About</a></div>
         </div>
         <div class="flex-1 flex items-center justify-end">
             <a href="https://github.com/anantnrg"><Icon name="octicon:mark-github-24" class="text-text" size="24px"/></a>
