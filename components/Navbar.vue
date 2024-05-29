979f9e14eede5fc83f2c617ab4cf3503eed2ca4e
Anant Narayan <anantnrg@gmail.com>
2024-05-29 17:28:42 +0530

    yay! werks!

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -8,7 +8,7 @@
             <div class="mr-6"><a href="#" class="font-sans font-400 text-text decoration-none">Project Series</a></div>
             <div><a href="#" class="font-sans font-400 text-text decoration-none">About</a></div>
         </div>
-        <div class="flex-1 items-center justify-end">
+        <div class="flex-1 flex items-center justify-end">
             <a href="https://github.com/anantnrg"><Icon name="octicon:mark-github-24" class="text-text"/></a>
         </div>
     </div>
