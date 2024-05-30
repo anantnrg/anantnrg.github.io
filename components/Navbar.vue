28f19e5c927bd55d649c2bb8c51884616abe52fb
Anant Narayan <anantnrg@gmail.com>
2024-05-30 20:23:16 +0530

    format

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,16 +1,21 @@
 <template>
     <header class="bg-base/75 backdrop-blur border-0 border-b border-solid border-surface0 sticky top-0 z-50">
-    <div class="mx-auto h-[64px] px-8 flex items-center justify-between gap-4">
-        <div class="flex-1"><span class="font-spaceGrotesk font-600 text-3xl text-accent">Anant Narayan</span></div>
-        <div class="w-auto h-full items-center justify-center flex">
-            <div class="mr-6"><a href="#" class="font-sans  text-text  decoration-none  hover:text-accent">Home</a></div>
-            <div class="mr-6"><a href="#" class="font-sans  text-text decoration-none hover:text-accent">Blog</a></div>
-            <div class="mr-6"><a href="#" class="font-sans  text-text decoration-none hover:text-accent">Project Series</a></div>
-            <div><a href="#" class="font-sans  text-text decoration-none hover:text-accent">About</a></div>
+        <div class="mx-auto h-[64px] px-8 flex items-center justify-between gap-4">
+            <div class="flex-1"><span class="font-spaceGrotesk font-600 text-3xl text-accent">Anant Narayan</span></div>
+            <div class="w-auto h-full items-center justify-center flex">
+                <div class="mr-6"><a href="#" class="font-sans  text-text  decoration-none  hover:text-accent">Home</a>
+                </div>
+                <div class="mr-6"><a href="#" class="font-sans  text-text decoration-none hover:text-accent">Blog</a>
+                </div>
+                <div class="mr-6"><a href="#" class="font-sans  text-text decoration-none hover:text-accent">Project
+                        Series</a></div>
+                <div><a href="#" class="font-sans  text-text decoration-none hover:text-accent">About</a></div>
+            </div>
+            <div class="flex-1 flex items-center justify-end">
+                <a href="https://github.com/anantnrg">
+                    <Icon name="octicon:mark-github-24" class="text-text" size="24px" />
+                </a>
+            </div>
         </div>
-        <div class="flex-1 flex items-center justify-end">
-            <a href="https://github.com/anantnrg"><Icon name="octicon:mark-github-24" class="text-text" size="24px"/></a>
-        </div>
-    </div>
-</header>
+    </header>
 </template>
