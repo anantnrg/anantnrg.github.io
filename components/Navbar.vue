1c5fc59440ccc233d395e12f62a0900932b6768b
Anant Narayan <anantnrg@gmail.com>
2024-05-30 20:21:25 +0530

    aaahhhh

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -3,10 +3,10 @@
     <div class="mx-auto h-[64px] px-8 flex items-center justify-between gap-4">
         <div class="flex-1"><span class="font-spaceGrotesk font-600 text-3xl text-accent">Anant Narayan</span></div>
         <div class="w-auto h-full items-center justify-center flex">
-            <div class="mr-6"><a href="#" class="font-sans  text-text text-sm decoration-none  hover:text-accent">Home</a></div>
-            <div class="mr-6"><a href="#" class="font-sans  text-text text-sm decoration-none hover:text-accent">Blog</a></div>
-            <div class="mr-6"><a href="#" class="font-sans  text-text text-sm decoration-none hover:text-accent">Project Series</a></div>
-            <div><a href="#" class="font-sans  text-text text-sm decoration-none hover:text-accent">About</a></div>
+            <div class="mr-6"><a href="#" class="font-sans  text-text  decoration-none  hover:text-accent">Home</a></div>
+            <div class="mr-6"><a href="#" class="font-sans  text-text decoration-none hover:text-accent">Blog</a></div>
+            <div class="mr-6"><a href="#" class="font-sans  text-text decoration-none hover:text-accent">Project Series</a></div>
+            <div><a href="#" class="font-sans  text-text decoration-none hover:text-accent">About</a></div>
         </div>
         <div class="flex-1 flex items-center justify-end">
             <a href="https://github.com/anantnrg"><Icon name="octicon:mark-github-24" class="text-text" size="24px"/></a>
