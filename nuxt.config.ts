3054cdb1099135b11722e933508207ed935d7a78
Anant Narayan <anantnrg@gmail.com>
2024-05-31 21:44:09 +0530

    switch to tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/eslint', '@unocss/nuxt', 'nuxt-icon'],
+  modules: ['@nuxt/content', '@nuxt/eslint', 'nuxt-icon', "@nuxtjs/tailwindcss"],
   vite: {
     css: {
       preprocessorOptions: {
