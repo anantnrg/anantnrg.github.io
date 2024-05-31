0abc73c91049afefd8cf3a82ab83b16825aa0f99
Anant Narayan <anantnrg@gmail.com>
2024-05-31 17:34:37 +0530

    fucking hell

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,19 +1,20 @@
 <template>
-    <header class="bg-base/75 backdrop-blur border-0 border-b border-solid border-surface0 sticky top-0 z-50">
+    <header class="bg-mantle/75 backdrop-blur border-0 border-b border-solid border-surface0 sticky top-0 z-50">
         <div class="mx-auto h-[64px] px-8 flex items-center justify-between gap-4">
             <div class="flex-1"><span class="font-spaceGrotesk font-600 text-3xl text-accent">Anant Narayan</span></div>
             <div class="w-auto h-full items-center justify-center flex">
                 <div class="mr-6"><a href="#"
-                        class="font-sans text-text text-sm font-500 decoration-none  hover:text-accent">Home</a>
+                        class="font-sans text-text text-sm/6 font-semibold decoration-none hover:text-accent transition-colors transition-duration-150">Home</a>
                 </div>
                 <div class="mr-6"><a href="#"
-                        class="font-sans text-text text-sm font-500 decoration-none hover:text-accent">Blog</a>
+                        class="font-sans text-text text-sm/6 font-semibold decoration-none hover:text-accent transition-colors transition-duration-150">Blog</a>
                 </div>
                 <div class="mr-6"><a href="#"
-                        class="font-sans text-text text-sm font-500 decoration-none hover:text-accent">Project
+                        class="font-sans text-text text-sm/6 font-semibold decoration-none hover:text-accent transition-colors transition-duration-150">Project
                         Series</a></div>
                 <div><a href="#"
-                        class="font-sans text-sm text-text font-500 decoration-none hover:text-accent">About</a></div>
+                        class="font-sans text-text text-sm/6 font-semibold decoration-none hover:text-accent transition-colors transition-duration-150">About</a>
+                </div>
             </div>
             <div class="flex-1 flex items-center justify-end">
                 <a href="https://github.com/anantnrg">
