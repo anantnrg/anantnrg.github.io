5d57e1d9776f90173d1719e757a875a2310f245b
Anant Narayan <anantnrg@gmail.com>
2024-06-07 15:02:03 +0530

    add dot bkg

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full h-full flex flex-col overflow-auto">
     <div class="w-full min-h-full relative select-none">
-      <div class="dot-bkg dark:dot-bkg-dark" />
+      <div class="absolute inset-0 h-full w-full bg-slate-100 dark:bg-slate-900 bg-[radial-gradient(#cbd5e1_2px,transparent_2px)] dark:bg-[radial-gradient(#1e293b_1px,transparent_1px)] [background-size:2rem_2rem]" />
     </div>
   </div>
 </template>
