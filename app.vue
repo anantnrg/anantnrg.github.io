e6e2bdfba90dc22dc48835953e5d8ea5107766c2
Anant Narayan <anantnrg@gmail.com>
2024-06-08 16:27:07 +0530

    try sticky again

--- app.vue
+++ app.vue
@@ -1,8 +1,8 @@
 <template>
   <div
-    class="antialiased w-full h-full flex flex-col bg-slate-100 text-slate-900 dark:bg-slate-900 dark:text-slate-200 relative overflow-hidden">
+    class="antialiased w-full h-full flex flex-col bg-slate-100 text-slate-900 dark:bg-slate-900 dark:text-slate-200 relative">
     <NavBar />
-    <NuxtPage class="w-full h-[calc(100%-65px)]"/>
+    <NuxtPage />
   </div>
 </template>
 
