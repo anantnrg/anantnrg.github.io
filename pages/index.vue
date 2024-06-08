fbd84f0198f026c78e5dd2131a3fb66aa33af8ad
Anant Narayan <anantnrg@gmail.com>
2024-06-08 16:42:48 +0530

    aahhh

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full h-full flex flex-col overflow-auto">
+  <div class="w-full h-full flex flex-col flex-grow">
     <div class="w-full min-h-full relative select-none">
       <div
         class="absolute inset-0 h-full w-full bg-slate-100 dark:bg-slate-900 bg-[radial-gradient(#64748b_1px,transparent_1px)] dark:bg-[radial-gradient(#334155_1px,transparent_1px)] [background-size:2rem_2rem]" />
