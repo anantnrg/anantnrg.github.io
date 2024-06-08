f1abf5c2fd4b70ca5dd8082a79e0bc6d0cdb4481
Anant Narayan <anantnrg@gmail.com>
2024-06-08 21:07:07 +0530

    aaahhh

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full h-full flex flex-col flex-grow">
-    <div class="w-full min-h-[200vh] relative select-none pt-[64px]">
+    <div class="w-full min-h-[200vh] relative select-none pt-[64px] flex flex-col">
       <div
         class="absolute inset-0 h-full w-full bg-slate-100 dark:bg-slate-900 bg-[radial-gradient(#64748b_1px,transparent_1px)] dark:bg-[radial-gradient(#334155_1px,transparent_1px)] [background-size:2rem_2rem]" />
       <div class="z-10">
