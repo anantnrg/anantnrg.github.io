5ff7c3a88a3b8ecf77a594c1f49299343e2f8b89
Anant Narayan <anantnrg@gmail.com>
2024-06-09 17:28:39 +0530

    back to work

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div class="w-full min-h-screen relative select-none pt-[64px] flex flex-col">
       <div
         class="absolute z-0 inset-0 h-full w-full bg-slate-100 dark:bg-slate-900 bg-[radial-gradient(#64748b_1px,transparent_1px)] dark:bg-[radial-gradient(#334155_1px,transparent_1px)] [background-size:2rem_2rem]" />
-      <div class="relative z-10 w-full h-full">
+      <div class="relative z-10 w-full h-full flex flex-col items-center justify-center">
         <h1>aasglaslgakgklasdaskl</h1>
       </div>
     </div>
