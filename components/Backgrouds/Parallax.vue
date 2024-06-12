b352dd6001b535c7fa66f5c2d41a58cf9ad286fc
Anant Narayan <anantnrg@gmail.com>
2024-06-12 21:40:35 +0530

    work

--- components/Backgrouds/Parallax.vue
+++ components/Backgrouds/Parallax.vue
@@ -4,7 +4,7 @@
   <div class="overflow-hidden">
     <div id="glow" class="z-1 absolute top-0 left-0 w-full h-full" />
     <div id="overlay" class="z-[3] opacity-80 absolute top-0 left-0 w-full h-full" />
-    <canvas id="canvas" />
+    <canvas id="canvas" class="absolute z-[2] top-0 left-0" />
   </div>
 </template>
 
