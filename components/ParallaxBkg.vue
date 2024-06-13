760363380040f42c9977ece57400f7cc75aa60b7
Anant Narayan <anantnrg@gmail.com>
2024-06-13 22:25:14 +0530

    hmmm

--- components/ParallaxBkg.vue
+++ components/ParallaxBkg.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <div class="absolute flex gap-x-[5%] items-center justify-center z-[2]">
+  <div class="w-full h-full">
+    <div class="w-full h-full absolute flex gap-x-[5%] items-center justify-center z-[2]">
       <div class="h-[60%] w-[20%] rounded-full bg-slate-500" />
     </div>
   </div>
