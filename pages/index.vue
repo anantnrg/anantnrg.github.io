db5ff87c0babc1455c60ac145700bc65d05a9a32
Anant Narayan <anantnrg@gmail.com>
2024-06-13 21:18:55 +0530

    rename

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full h-full flex flex-col flex-grow">
     <div class="w-full min-h-screen relative select-none pt-[64px] flex flex-col">
-      <Parallax />
+      <ParallaxBkg />
       <div class="relative z-10 w-full h-full flex flex-col items-center justify-center">
         <h1>aasglaslgakgklasdaskl</h1>
       </div>
