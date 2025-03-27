0f4bfd8431d3b0594506628c4986ba6aa34d1e41
Anant Narayan <anantnrg@gmail.com>
2025-03-27 12:33:21 +0530

    aahh

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
+import markdownIntegration from "@astropub/md";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [markdownIntegration()],
   vite: {
     plugins: [tailwindcss(), mdx()],
   },
--- src/pages/categories.ts
+++ src/pages/categories.ts
@@ -2,5 +2,6 @@ export const categoryData = {
   "embedded-rust-stm32": {
     name: "Embedded Rust on an STM32",
     image: "/images/categories/embedded-rust-stm32-cover.png",
+    desc: "This series dives into **Embedded Rust**, leveraging `no_std` to write bare-metal firmware for the **STM32 Blue Pill**. It covers **setting up the development environment**, flashing code onto the board, and understanding low-level microcontroller operation. We’ll navigate **register-level programming**, work with **sensors, servos, and peripherals**, and generate **PWM signals** for precise control. If you’re into **low-level performance, deterministic systems, and real-time applications**, this is where Rust meets raw silicon—no OS, no hand-holding, just embedded engineering done right.",
   },
 };
